8dbc2f0e0c874eb55df4adaa345ec6ca480691f6
Joffry Ferrater <joffry.ferrater@axiomatics.com>
2019-01-02 15:03:19 +0100

    clean up

--- abac-pep-spring-security/src/main/java/com/github/joffryferrater/pep/PepConfiguration.java
+++ abac-pep-spring-security/src/main/java/com/github/joffryferrater/pep/PepConfiguration.java
@@ -2,16 +2,12 @@ package com.github.joffryferrater.pep;
 
 import com.github.joffryferrater.pep.client.PdpClient;
 import com.github.joffryferrater.pep.configuration.PdpConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PepConfiguration {
-
-    @Autowired
-    private PdpConfiguration pdpConfiguration;
 
     @Bean
     public PdpConfiguration pdpConfiguration() {
@@ -21,10 +17,11 @@ public class PepConfiguration {
     @Bean
     public PdpClient pdpClient(RestTemplateBuilder restTemplateBuilder) {
         restTemplateBuilder.setConnectTimeout(5000);
+        final PdpConfiguration pdpConfiguration = pdpConfiguration();
         final String username = pdpConfiguration.getUsername();
         final String password = pdpConfiguration.getPassword();
         if (username != null && password != null) {
-            restTemplateBuilder.basicAuthorization(pdpConfiguration.getUsername(), pdpConfiguration.getPassword());
+            restTemplateBuilder.basicAuthorization(username, password);
         }
         return new PdpClient(restTemplateBuilder, pdpConfiguration());
     }
