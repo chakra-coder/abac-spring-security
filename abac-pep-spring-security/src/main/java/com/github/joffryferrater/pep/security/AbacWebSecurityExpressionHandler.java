08e4489facb98db8710ddbb972d2587c8740f11d
Joffry Ferrater <joffry.ferrater@axiomatics.com>
2019-01-07 12:34:24 +0100

    Annotate  @Configuration in abacwebexpressionhandler

--- abac-pep-spring-security/src/main/java/com/github/joffryferrater/pep/security/AbacWebSecurityExpressionHandler.java
+++ abac-pep-spring-security/src/main/java/com/github/joffryferrater/pep/security/AbacWebSecurityExpressionHandler.java
@@ -2,11 +2,13 @@ package com.github.joffryferrater.pep.security;
 
 import com.github.joffryferrater.pep.client.PdpClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
+@Configuration
 public class AbacWebSecurityExpressionHandler extends DefaultWebSecurityExpressionHandler {
 
     @Autowired
